bc28c94951ddc532a9f53f7253142363babd0b13
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2022-08-16 14:34:00 +0200

    Add support for duration columns

--- mssev/module.py
+++ mssev/module.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from pandas.api.types import is_timedelta64_dtype
 
 
 def _load_global_msss_table(path):
@@ -35,6 +36,8 @@ def global_msss(df, edss='edss', duration='dd'):
         GLOBAL_MSSS = _load_global_msss_table(path)
 
     df = df.copy()
+    if is_timedelta64_dtype(df[duration]):
+        df[duration] = df[duration].dt.days / 365.25
     df[duration] = np.floor(df[duration]).clip(upper=30).astype('Int32')
     results = df.merge(GLOBAL_MSSS, left_on=[duration, edss], right_index=True, how='left')
     return results.MSSS
@@ -47,6 +50,8 @@ def global_armss(df, edss='edss', age='ageataedss'):
         GLOBAL_ARMSS = _load_global_armss_table(path)
 
     df = df.copy()
+    if is_timedelta64_dtype(df[age]):
+        df[age] = df[age].dt.days / 365.25
     df[age] = np.floor(df[age]).clip(upper=75).astype('Int32')
     results = df.merge(GLOBAL_ARMSS, left_on=[age, edss], right_index=True, how='left')
     return results.ARMSS
